93af2def69ca8c74ac945c61caba40542d8fbcb7
juulietafigueroa <maria.figueroa@mi.unc.edu.ar>
2022-06-28 23:14:35 -0300

    first commit

--- Proyecto/App/views.py
+++ Proyecto/App/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
 
 # Create your views here.
+
+def home(request):
+  return render(request, 'App/home.html')
+
+def Historia_comunidad(request):
+    return render(request, 'App/Historia_comu.html')
+
+def Actividades_comunidad(request):
+    return render(request, 'App/Actividades_comu.html')
+
+def Informacion_sobre_mi(request):
+    return render(request, 'App/Informacion_sobre_mi.html')
+
+#def home(self):
+ #   plantilla = loader.get_template('home.html')
+  #  documento = plantilla.render()
+   # return HttpResponse(documento)
